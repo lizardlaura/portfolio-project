71b8cc4bfcf456e94bdfb617353833e926164277
lizardlaura <lauraassaidi@gmail.com>
2026-03-09 15:14:41 -0700

    made example site mobile friendly

--- src/components/party-page-component/PartyHero.tsx
+++ src/components/party-page-component/PartyHero.tsx
@@ -1,56 +1,49 @@
 import disco from "../../assets/images/partyplan/disco.avif";
 import balloons from "../../assets/images/partyplan/balloons.avif";
-import event from '../../assets/images/partyplan/event.avif'
+import event from "../../assets/images/partyplan/event.avif";
 import { useState } from "react";
 const PartyHero = () => {
-
-  const [activeImg, setActiveImg] = useState(0) //0 | 1 | 2
-
+  const [activeImg, setActiveImg] = useState(0); //0 | 1 | 2
+  const imageDetails = [
+    { source: disco, alt: "disco balls", title: "Theme Parties" },
+    {
+      source: balloons,
+      alt: "balloons floating against ceiling",
+      title: "Birthdays",
+    },
+    {
+      source: event,
+      alt: "people raising champagne glasses at dinner party",
+      title: "Events",
+    },
+  ];
   return (
-    <div className="max-h-[75vh] flex gap-0 justify-center text-white">
-      <div className={`${activeImg===0?'max-w-[60vw]' : 'max-w-[20vw]'} overflow-hidden relative transition-all duration-500`} onMouseEnter={()=>setActiveImg(0)}>
-        
-        <img
-        src={disco}
-        alt="Disco balls"
-        className="w-auto h-full object-cover max-h-[75vh]"
-      />
-      <div className={`${activeImg === 0 ? 'opacity-0':'opacity-100'} bg-orange-600 h-full w-full absolute top-0 left-0 transition-opacity duration-500`} />
-      <div className="absolute bottom-2 right-2 text-2xl p-2">
-          <span style={{textShadow: '0 0 3px rgb(0, 0, 0), 0 0 3px rgb(0, 0, 0)'}}>
-          Theme Parties
-          </span>
+    <div className="party-hero-container">
+      {imageDetails.map((img, index) => (
+        <div
+          className={`${activeImg === index ? "md:max-w-[60vw] max-w-[100vw]" : "md:max-w-[20vw] max-w-[100vw]"} bg-green-500 overflow-hidden relative transition-all duration-500`}
+          onMouseEnter={() => setActiveImg(index)}
+        >
+          <img
+            src={img.source}
+            alt={img.alt}
+            className="w-auto object-cover h-[75vh]"
+          />
+          <div
+            className={`${activeImg === index ? "opacity-0" : "md:opacity-100"} opacity-0 ${index == 0 ? "bg-orange-600" : index === 1 ? "bg-green-600" : "bg-purple-600"} h-full w-full absolute top-0 left-0 transition-opacity duration-500`}
+          />
+          <div className="absolute bottom-2 right-2 text-2xl p-2">
+            <span
+              style={{
+                textShadow: "0 0 3px rgb(0, 0, 0), 0 0 3px rgb(0, 0, 0)",
+              }}
+            >
+              {img.title}
+            </span>
+          </div>
         </div>
-      </div>
-      
-      <div className={`${activeImg===1?'max-w-[60vw]' : 'max-w-[20vw]'} overflow-hidden relative transition-all duration-500 `} onMouseEnter={()=>setActiveImg(1)}>
-        
-        <img
-          src={balloons}
-          alt="balloons floating against ceiling"
-          className="w-auto h-[75vh] object-cover"
-        />
-        <div className={`${activeImg === 1 ? 'opacity-0':'opacity-100'} bg-green-600 h-full w-full absolute top-0 left-0 transition-opacity duration-500`} />
-        <div className="absolute bottom-2 right-2 text-2xl p-2">
-          <span style={{textShadow: '0 0 3px rgb(0, 0, 0), 0 0 3px rgb(0, 0, 0)'}}>
-          Birthdays
-          </span>
-        </div>
-      </div>
-       <div className={`${activeImg===2?'max-w-[60vw]' : 'max-w-[20vw]'} overflow-hidden relative transition-all duration-500`}>
-        
-        <img
-          src={event}
-          alt="people raising up champagne glasses at dinner party"
-          className="w-auto h-[75vh] object-cover"
-        />
-        <div className={`${activeImg === 2 ? 'opacity-0':'opacity-100'} bg-purple-600 h-full w-full absolute top-0 left-0 transition-opacity duration-500`} onMouseEnter={()=>setActiveImg(2)}/>
-        <div className="absolute bottom-2 right-2 text-2xl p-2">
-          <span style={{textShadow: '0 0 3px rgb(0, 0, 0), 0 0 3px rgb(0, 0, 0)'}}>
-          Special Events
-          </span>
-        </div>
-      </div>
+      ))}
+     
     </div>
   );
 };
